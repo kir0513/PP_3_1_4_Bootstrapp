688313acb01b94f45abd9c05cdbbcdca27f2417b
kir0513 <kir0513@yandex.ru>
2023-05-29 19:00:20 +0300

    Необходимо перенести CRUD-приложение на Spring Boot.

---
+++ src/main/java/com/example/PP_2_3_1_SpringMVC_Hibernate_Boot/Pp231SpringMvcHibernateBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PP_2_3_1_SpringMVC_Hibernate_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp231SpringMvcHibernateBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp231SpringMvcHibernateBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PP_2_3_1_SpringMVC_Hibernate_Boot/Pp231SpringMvcHibernateBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PP_2_3_1_SpringMVC_Hibernate_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp231SpringMvcHibernateBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
