b24fef059f091a5becec2cc8f8c8239fd166ddc3
kir0513 <kir0513@yandex.ru>
2023-06-20 20:33:14 +0300

    Контроллер для админа вынесен в отдельный класс

--- src/main/java/com/example/PP_2_3_1_SpringMVC_Hibernate_Boot/controller/UserController.java
+++ src/main/java/com/example/PP_2_3_1_SpringMVC_Hibernate_Boot/controller/UserController.java
@@ -39,13 +39,13 @@ public class UserController {
         model.addAttribute("user", user);
         return "user/curr_user_info";
     }
-    @GetMapping(value = "/admin")
-    public String index(ModelMap model) {
-        List<User> list = userService.getUsers();
-        model.addAttribute("listUsers", list);
-        System.out.println("Переход по / на /index.html");
-        return "admin/list_of_users";
-    }
+//    @GetMapping(value = "/admin")
+//    public String index(ModelMap model) {
+//        List<User> list = userService.getUsers();
+//        model.addAttribute("listUsers", list);
+//        System.out.println("Переход по / на /index.html");
+//        return "admin/list_of_users";
+//    }
 
     @GetMapping(value = "/login")
     public String getLoginPage() {
@@ -62,51 +62,51 @@ public class UserController {
 
 
 
-    @GetMapping(value = "/admin/list_users")
-    public String showAllUsers(ModelMap model) {
-        List<User> list = userService.getUsers();
-        model.addAttribute("listUsers", list);
-        System.out.println("Открытие /admin/list_users (pages/list_of_users.html)");
-        return "admin/list_of_users";
-    }
+//    @GetMapping(value = "/admin/list_users")
+//    public String showAllUsers(ModelMap model) {
+//        List<User> list = userService.getUsers();
+//        model.addAttribute("listUsers", list);
+//        System.out.println("Открытие /admin/list_users (pages/list_of_users.html)");
+//        return "admin/list_of_users";
+//    }
 
-    @GetMapping(value = "/admin/show_single_user")
-    public String showSingleUser (@RequestParam(value = "id") Long id, Model model) {
-        model.addAttribute("user", userService.getSingleUserById(id));
-        return "admin/show_single_user_info";
-    }
+//    @GetMapping(value = "/admin/show_single_user")
+//    public String showSingleUser (@RequestParam(value = "id") Long id, Model model) {
+//        model.addAttribute("user", userService.getSingleUserById(id));
+//        return "admin/show_single_user_info";
+//    }
 
-    @GetMapping("/admin/add_user")
-    public String addUser(Model model){
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "admin/form_add_user";
-    }
-    @PostMapping("/admin/save_or_update_user")
-    public String saveNewOrUpdateExistUser(@ModelAttribute("user") User user,
-             @RequestParam(value = "selectedRoles", required = false) String[] selectedRoles){
-        if (selectedRoles != null) {
-            Set<Role> roles = new HashSet<>();
-            for (String elemArrSelectedRoles : selectedRoles) {
-                roles.add(roleService.getRoleByName(elemArrSelectedRoles));
-            }
-            user.setRoles(roles);
-        }
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
+//    @GetMapping("/admin/add_user")
+//    public String addUser(Model model){
+//        model.addAttribute("user", new User());
+//        model.addAttribute("roles", roleService.getAllRoles());
+//        return "admin/form_add_user";
+//    }
+//    @PostMapping("/admin/save_or_update_user")
+//    public String saveNewOrUpdateExistUser(@ModelAttribute("user") User user,
+//             @RequestParam(value = "selectedRoles", required = false) String[] selectedRoles){
+//        if (selectedRoles != null) {
+//            Set<Role> roles = new HashSet<>();
+//            for (String elemArrSelectedRoles : selectedRoles) {
+//                roles.add(roleService.getRoleByName(elemArrSelectedRoles));
+//            }
+//            user.setRoles(roles);
+//        }
+//        userService.saveUser(user);
+//        return "redirect:/admin";
+//    }
 
-    @GetMapping("/admin/edit_user")
-    public String edit(@RequestParam(value = "id") Long id, Model model) {
-        model.addAttribute("user", userService.getSingleUserById(id));
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "admin/form_edit_user";
-    }
-
-    @GetMapping(value = "/admin/delete_user")
-    public String deleteUser (@RequestParam(value = "id") Long id, Model model) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
+//    @GetMapping("/admin/edit_user")
+//    public String edit(@RequestParam(value = "id") Long id, Model model) {
+//        model.addAttribute("user", userService.getSingleUserById(id));
+//        model.addAttribute("roles", roleService.getAllRoles());
+//        return "admin/form_edit_user";
+//    }
+//
+//    @GetMapping(value = "/admin/delete_user")
+//    public String deleteUser (@RequestParam(value = "id") Long id, Model model) {
+//        userService.deleteUser(id);
+//        return "redirect:/admin";
+//    }
 
 }
